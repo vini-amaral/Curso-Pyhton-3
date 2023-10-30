9a0b2835b29e49103f6c9f9a66433b20535820b5
Vinicius Alves do Amaral <vialama@gmail.com>
2023-10-30 15:25:01 -0300

    Criação de Property e Setter para Conta

--- Orientação a Objetos/conta.py
+++ Orientação a Objetos/conta.py
@@ -1,14 +1,15 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite) -> None:
-        #print("Construindo o objeto {}".format(self))
+        # print("Construindo o objeto {}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
     def extrato(self):
-        print("O saldo o titular {} é de {} e o limite disponível é de {}".format(self.__titular, self.__saldo, self.__limite))
+        print("O saldo o titular {} é de {} e o limite disponível é de {}".format(
+            self.__titular, self.__saldo, self.__limite))
 
     def depositar(self, valor):
         if (valor > 0):
@@ -35,8 +36,22 @@ class Conta:
             print(
                 "O valor desejado é maior que o saldo em conta e o limite disponível.\nSaque não realizado.")
 
-
-
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
--- Orientação a Objetos/teste.py
+++ Orientação a Objetos/teste.py
@@ -3,11 +3,8 @@ from conta import Conta
 conta1 = Conta(123, "Ana", 60.0, 500.0)
 conta2 = Conta(456, "Bia", 30.0, 500.0)
 
-conta1.extrato()
-conta2.extrato()
 
-conta1.transferir(15, conta2)
+print(conta1.limite)
+conta1.limite = 900.0
 
-conta1.extrato()
-conta2.extrato()
-
+print(conta1.limite)
