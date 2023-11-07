c3c2306ec56e45074eaccf3bd2afa239be96aa46
Vinicius Alves do Amaral <vialama@gmail.com>
2023-11-07 13:15:29 -0300

    Criação e chamada de métodos estáticos

--- Orientação a Objetos/teste.py
+++ Orientação a Objetos/teste.py
@@ -1,11 +1,17 @@
 from conta import Conta
 
 conta1 = Conta(123, "Ana", 50.0, 500.0)
+print(conta1.codigo_banco())
 
-conta1.sacar(50)
+print(Conta.codigos_bancos())
+print(Conta.codigos_bancos()["BB"])
+print(Conta.codigos_bancos()["Caixa"])
+print(Conta.codigos_bancos()["Bradesco"])
 
-conta1.sacar(350)
+# conta1.sacar(50)
 
-conta1.sacar(150)
+# conta1.sacar(350)
 
-conta1.sacar(500)
+# conta1.sacar(150)
+
+# conta1.sacar(500)
